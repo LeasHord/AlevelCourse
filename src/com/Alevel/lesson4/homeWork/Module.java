b93db039c54cb502c6dcc5f6ccb3a0529495525c
Leas Hordynets <38952249+LeasHord@users.noreply.github.com>
2019-01-22 21:42:54 +0200

    new homewrok

---
+++ src/com/Alevel/lesson4/homeWork/Module.java
@@ -0,0 +1,29 @@
+package com.Alevel.lesson4.homeWork;
+
+public class Module {
+
+    public static void main(String[] args) {
+        System.out.print(singleNumbers(1, 3, 5, 1, 3, 2, 7, 8, 9, 0, 2));
+        System.out.println();
+    }
+
+    public static int singleNumbers(int...nums) {
+
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            int repeat = 0;
+            for(int j = 0; j < i; j++){
+                if(nums[j] == nums[i]){
+                    repeat ++;
+                }
+            }
+            if(repeat == 0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+
---
+++ src/com/Alevel/lesson4/homeWork/StringFromLetters.java
@@ -0,0 +1,23 @@
+package com.Alevel.lesson4.homeWork;
+
+public class StringFromLetters {
+    public static void main(String[] args) {
+        System.out.println(letters("m a m a s i t a o u r h o m e"));
+    }
+
+    public static String letters(String letters) {
+        String[] newLetters = letters.split(" ");
+        String finalString = "";
+        for (int i = 0; i < newLetters.length; i++) {
+            for (int j = 0; j < i; j++) {
+
+                while (j < 3) {
+                    finalString += newLetters[(int) (Math.random() * newLetters.length)];
+                    j++;
+                }
+            }                     finalString+=" ";
+
+        }
+        return finalString;
+    }
+}
