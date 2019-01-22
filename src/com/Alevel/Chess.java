5f1ddd521fb28829d0a2b89367f5cafe487e6eed
Leas Hordynets <38952249+LeasHord@users.noreply.github.com>
2019-01-22 22:36:49 +0200

    all new files

---
+++ src/com/Alevel/Chess.java
@@ -0,0 +1,19 @@
+package com.Alevel;
+
+public class Chess {
+    public static void main(String[] args) {
+            System.out.println(chess(2, 1, 4, 3));
+        }
+
+        public static boolean chess(int x_position, int y_position, int nextStep1, int nextStep2) {
+            boolean step = false;
+            int step1 = 2;
+            int step2 = 1;
+            if (x_position + step1 == nextStep1 ^ x_position + step2 == nextStep1 && y_position + step1 == nextStep2 ^ y_position + step2 == nextStep2) {
+                step = true;
+            }
+
+            return step;
+        }
+    }
+
--- src/com/Alevel/StringFromLetters.java
+++ src/com/Alevel/StringFromLetters.java
@@ -1,4 +1,4 @@
-package com.Alevel.lesson4.homeWork;
+package com.Alevel;
 
 public class StringFromLetters {
     public static void main(String[] args) {
@@ -8,16 +8,19 @@ public class StringFromLetters {
     public static String letters(String letters) {
         String[] newLetters = letters.split(" ");
         String finalString = "";
-        for (int i = 0; i < newLetters.length; i++) {
-            for (int j = 0; j < i; j++) {
 
-                while (j < 3) {
-                    finalString += newLetters[(int) (Math.random() * newLetters.length)];
-                    j++;
+        for(int i = 0; i < newLetters.length; ++i) {
+            for(int j = 0; j < i; ++j) {
+                while(j < 3) {
+                    finalString = newLetters[(int)(Math.random() * (double)newLetters.length)];
+                    ++j;
                 }
-            }                     finalString+=" ";
+            }
 
+            finalString = finalString;
         }
+
         return finalString;
     }
 }
+
