aced60bcfe8b31881210aad3750c92fb30656f59
Leas Hordynets <38952249+LeasHord@users.noreply.github.com>
2019-02-21 12:36:30 +0200

    Multiplication

--- src/com/Alevel/homeWorks/Multiple1.java
+++ src/com/Alevel/homeWorks/Multiple1.java
@@ -1,18 +1,27 @@
 package com.Alevel.homeWorks;
 
-import java.util.Arrays;
 
 public class Multiple1 {
     public static void main(String[] args) {
-        int firstArr[] = {1, 2, 3};
-        int secondArr[] = {7, 5};
-        System.out.print(Arrays.toString(multiple(firstArr, secondArr)));
+
+        int i = 25;
+        int j = 25;
+        System.out.println(multiple(i,j));
+
     }
 
-    public static int[] multiple(int[] first, int[] second) {
-        int result[] = new int[first.length + second.length];
+    public static int multiple(int x, int y) {
+        int result;
 
-        return result;
+        if (y == 0) {
+            return 0;
+        }
+        result = multiple(x, y/2);
+        if(y%2==0){
+            return result*2;
+        } else return x+(result*2);
+
     }
+
 }
 
