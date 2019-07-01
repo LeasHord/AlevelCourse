720d7e2573d4ecab5cdfd328861310e9b803ff1e
Leas Hordynets <38952249+LeasHord@users.noreply.github.com>
2019-07-01 14:55:17 +0300

    users

---
+++ UsersForCoffeeIN/src/main/java/coffee/demo/CoffeSearchUsersApplication.java
@@ -0,0 +1,13 @@
+package coffee.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeSearchUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeSearchUsersApplication.class, args);
+    }
+
+}
---
+++ UsersForCoffeeIN/src/main/java/entities/User.java
@@ -0,0 +1,92 @@
+package entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users", catalog = "coffeinusers")
+public class User {
+
+
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private long id;
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+    @Column(name = "firstname", nullable = false)
+    private String firstName;
+
+    @Column(name = "lastname", nullable = false)
+    private String lastNAme;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "status", nullable = false)
+    private String status;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastNAme() {
+        return lastNAme;
+    }
+
+    public void setLastNAme(String lastNAme) {
+        this.lastNAme = lastNAme;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
